2fcfd10d13019947f723e1686404bf3606fafb7c
Dzimeomeo <vydinh06002@gmail.com>
2025-10-23 08:07:11 +0700

    nop lab 8

--- lab5_module.py
+++ lab5_module.py
@@ -2,40 +2,40 @@
 ##==========Attributes================##
 class SanPham: 
     def __init__(self, ten_san_pham, gia, giam_gia):
-        self.ten_san_pham = ten_san_pham
-        self.gia = gia
-        self.giam_gia = giam_gia 
+        self.__ten_san_pham = ten_san_pham
+        self.__gia = gia
+        self.__giam_gia = giam_gia 
 
 ##============ Getter and Setter =============##
     def get_ten_san_pham(self):
-        return self.ten_san_pham
-    def set_ten_san_pham(self, ten_san_pham):
-        self.ten_san_pham = ten_san_pham
+        return self.__ten_san_pham
+    def set_ten_san_pham(self, __ten_san_pham):
+        self.__ten_san_pham = __ten_san_pham
     
     def get_gia(self):
-        return self.gia
+        return self.__gia
     def set_gia(self, gia):
-        self.gia = gia
+        self.__gia = gia
 
     def get_giam_gia(self):
-        return self.giam_gia
+        return self.__giam_gia
     def set_giam_gia(self, giam_gia):
-        self.giam_gia = giam_gia
+        self.__giam_gia = giam_gia
 
         
 #============ Methods =============##
     def nhap_san_pham(self):
-        self.ten_san_pham = input("Nhập tên sản phẩm: ")
-        self.gia = float(input("Nhập giá sản phẩm: "))
-        self.giam_gia = float(input("Nhập phần trăm giảm giá (1-100): ")) / 100
+        self.__ten_san_pham = input("Nhập tên sản phẩm: ")
+        self.__gia = float(input("Nhập giá sản phẩm: "))
+        self.__giam_gia = float(input("Nhập phần trăm giảm giá (1-100): ")) / 100
     
 
-    def thue_nhap_khau(self):
-        return self.gia * 0.1
-    def tong_thanh_toan(self):
-        giam = self.gia * self.giam_gia
-        thue = self.thue_nhap_khau()
-        tong = self.gia - giam + thue
+    def __thue_nhap_khau(self):
+        return self.__gia * 0.1
+    def __tong_thanh_toan(self):
+        giam = self.__gia * self.__giam_gia
+        thue = self.__thue_nhap_khau()
+        tong = self.__gia - giam + thue
         return tong
     def xuat_san_pham(self):
-        print(f"Tên sản phẩm: {self.ten_san_pham}, có giá: {self.gia}, giảm giá: {self.giam_gia}, thuế nhập khẩu: {self.thue_nhap_khau()}, tổng thành toán: {self.tong_thanh_toan()}")
+        print(f"Tên sản phẩm: {self.__ten_san_pham}, có giá: {self.__gia}, giảm giá: {self.__giam_gia}, thuế nhập khẩu: {self.__thue_nhap_khau()}, tổng thành toán: {self.tong_thanh_toan()}")
